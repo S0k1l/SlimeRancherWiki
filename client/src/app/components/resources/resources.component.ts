c75afe1b586030109dcab2c6f049b2ef38b08eef
S0k1l <martynuky16@gmail.com>
2024-08-20 17:24:24 +0300

    Added Plorts

--- client/src/app/components/resources/resources.component.ts
+++ client/src/app/components/resources/resources.component.ts
@@ -50,4 +50,25 @@ export class ResourcesComponent {
     {id: '10', name: 'Старенька курочка', imgUrl: 'meats/Elder_Hen.webp'},
     {id: '9', name: 'Старенький півень', imgUrl: 'meats/Elder_Roostro.webp'},
   ]
+
+  plorts: Items[] = [
+    {id: '1', name: 'Рожевий плорт', imgUrl: 'plorts/PlortPINK.webp'},
+    {id: '2', name: 'Кам\'яний плорт', imgUrl: 'plorts/PlortROCK.webp'},
+    {id: '3', name: 'Смугастий плорт', imgUrl: 'plorts/PlortTABBY.webp'},
+    {id: '4', name: 'Фосфорний плорт', imgUrl: 'plorts/PlortPHOSPHOR.webp'},
+    {id: '5', name: 'Рад-плорт', imgUrl: 'plorts/PlortRAD.webp'},
+    {id: '6', name: 'Бум-плорт', imgUrl: 'plorts/PlortBOOM.webp'},
+    {id: '7', name: 'Медовий плорт', imgUrl: 'plorts/PlortHONEY.webp'},
+    {id: '8', name: 'Калюжний плорт', imgUrl: 'plorts/PlortPUDDLE.webp'},
+    {id: '9', name: 'Кристалевий плорт', imgUrl: 'plorts/PlortCRYSTAL.webp'},
+    {id: '10', name: 'Мисливець-плорт', imgUrl: 'plorts/PlortHUNTER.webp'},
+    {id: '11', name: 'Квантовий плорт', imgUrl: 'plorts/PlortQUANTUM.webp'},
+    {id: '12', name: 'Вогняний плорт', imgUrl: 'plorts/PlortFIRE.webp'},
+    {id: '13', name: 'Вихорний плорт', imgUrl: 'plorts/PlortDERVISH.webp'},
+    {id: '14', name: 'Ліанний плорт', imgUrl: 'plorts/PlortTANGLE.webp'},
+    {id: '15', name: 'Мозаїчний плорт', imgUrl: 'plorts/PlortMOSAIC.webp'},
+    {id: '16', name: 'Саблезубий плорт', imgUrl: 'plorts/PlortSABER.webp'},
+    {id: '17', name: 'Ртутний плорт', imgUrl: 'plorts/PlortQUICKSILVER.webp'},
+    {id: '18', name: 'Золотий плорт', imgUrl: 'plorts/PlortGOLD.webp'},
+  ]
 }
