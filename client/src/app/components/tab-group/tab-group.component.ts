9aee640309d15433125e3c41fadc98db6ff2f41a
S0k1l <martynuky16@gmail.com>
2024-08-20 18:17:15 +0300

    Added Slime Toys

--- client/src/app/components/tab-group/tab-group.component.ts
+++ client/src/app/components/tab-group/tab-group.component.ts
@@ -3,11 +3,12 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { SlimesComponent } from '../slimes/slimes.component';
 import { LocationsComponent } from '../locations/locations.component';
 import { ResourcesComponent } from '../resources/resources.component';
+import { ToysComponent } from "../toys/toys.component";
 
 @Component({
   selector: 'app-tab-group',
   standalone: true,
-  imports: [MatTabsModule, SlimesComponent, LocationsComponent, ResourcesComponent],
+  imports: [MatTabsModule, SlimesComponent, LocationsComponent, ResourcesComponent, ToysComponent],
   templateUrl: './tab-group.component.html',
   styleUrl: './tab-group.component.css'
 })
---
+++ client/src/app/components/toys/toys.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ItemButtonComponent } from '../item-button/item-button.component';
+import { CommonModule } from '@angular/common';
+
+interface Items {
+  id: string,
+  name: string,
+  imgUrl: string,
+}
+
+
+@Component({
+  selector: 'app-toys',
+  standalone: true,
+  imports: [MatTabsModule, ItemButtonComponent, CommonModule],
+  templateUrl: './toys.component.html',
+  styleUrl: './toys.component.css'
+})
+export class ToysComponent {
+
+  toys: Items[] = [
+    {id: '1', name: 'Пляжний м\'яч', imgUrl: 'toys/Beach_Ball.webp'},
+    {id: '2', name: 'Великий камінь', imgUrl: 'toys/Big_Rock.webp'},
+    {id: '3', name: 'Клубок пряжі', imgUrl: 'toys/Yarn_Ball.webp'},
+    {id: '4', name: 'Нічник', imgUrl: 'toys/Night_Light.webp'},
+    {id: '5', name: 'Енергоблок', imgUrl: 'toys/Power_Cell.webp'},
+    {id: '6', name: 'Вибуховий м\'яч', imgUrl: 'toys/Bomb_Ball.webp'},
+    {id: '7', name: 'Жужільна бджілка', imgUrl: 'toys/Buzzy_Bee.webp'},
+    {id: '8', name: 'Гумова качечка', imgUrl: 'toys/Rubber_Ducky.webp'},
+    {id: '9', name: 'Кришталева куля', imgUrl: 'toys/Crystal_Ball.webp'},
+    {id: '10', name: 'Плюшева курочка', imgUrl: 'toys/Stuffed_Chicken.webp'},
+    {id: '11', name: 'Кубик-загадка', imgUrl: 'toys/Puzzle_Cube.webp'},
+    {id: '12', name: 'Вугільна цеглина', imgUrl: 'toys/Charcoal_Brick.webp'},
+    {id: '13', name: 'Гіроскопічна дзиґа', imgUrl: 'toys/Gyro_Top.webp'},
+    {id: '14', name: 'Сонячний друг', imgUrl: 'toys/Sol_Mate.webp'},
+    {id: '15', name: 'Дискокуля', imgUrl: 'toys/Disco_Ball.webp'},
+    {id: '16', name: 'Стего-друг', imgUrl: 'toys/Stego_Buddy.webp'},
+  ]
+}
